1f9c0d1ad8c9f62948b83c743b74c6b1b2bed2c8
omarbadrandev <omarbadranm93@hotmail.com>
2022-03-17 15:27:17 +0100

    init

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavigationAppBar from "../components/DefaultPageLayout";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <NavigationAppBar />
+      test
+    </div>
+  );
+}
